1a488069016db343c6d83f6b44cf3de493a3c088
isabelhb <isabelbohre@gmail.com>
2024-04-25 09:20:46 +0200

    Initial commit

---
+++ src/app/reservation-overview/reservation-overview.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reservation-overview',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reservation-overview.component.html',
+  styleUrl: './reservation-overview.component.scss'
+})
+export class ReservationOverviewComponent {
+
+}
