18a7016ddda230654f6330c34ef9381e72ae6d85
isabelhb <isabelbohre@gmail.com>
2024-04-25 10:50:03 +0200

    FEAT: Hardcoded reservation detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AppLayoutComponent} from "./app-layout/app-layout.component";
+import {ReservationDetailsComponent} from "./reservation-details/reservation-details.component";
 import { CreateReservationComponent } from './reservation/create/create.component';
 
 export const routes: Routes = [
@@ -11,6 +12,10 @@ export const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent
+      },
+      // TODO add id of reservation in path
+      {path: 'details',
+      component: ReservationDetailsComponent
       },
       {
         path: 'reservation/create',
