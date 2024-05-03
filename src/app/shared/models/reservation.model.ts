525fa987e5c82d33f6dcf32255038faa15d53548
isabelhb <isabelbohre@gmail.com>
2024-05-03 15:58:47 +0200

    FEAT: dynamically display the details of reservation by id in url params, updated res details

--- src/app/shared/models/reservation.model.ts
+++ src/app/shared/models/reservation.model.ts
@@ -1,18 +1,26 @@
 import {Time} from "@angular/common";
+import {locationsModel} from "./locations.model";
 
-export class ReservationModel{
-  id: string;
-  location: Location;
-  type: string;
-  startTime: Time;
-  date: Date;
+export class ReservationModel {
+    id: string;
+    location: locationsModel;
+    wing: string;
+    floor: number;
+    room: string;
+    type: string;
+    startTime: Time;
+    date: Date;
 
-  constructor(id: string, location: Location, type: string, startTime: Time, date: Date){
-    this.id = id;
-    this.location = location;
-    this.type = type;
-    this.startTime = startTime;
-    this.date = date;
-  }
+    constructor(id: string, location: locationsModel, wing: string, floor: number, room: string,
+                type: string, startTime: Time, date: Date) {
+        this.id = id;
+        this.location = location;
+        this.wing = wing;
+        this.floor = floor;
+        this.room = room;
+        this.type = type;
+        this.startTime = startTime;
+        this.date = date;
+    }
 
 }
