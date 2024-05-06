c1149d56f59e36c205f56854cec433b746038af4
Amber de Geus <s1143282@student.hsleiden.nl>
2024-05-06 17:41:30 +0200

    feat: added a scroll arrow instead of a scrollbar

--- src/app/home/inbox/inbox.component.ts
+++ src/app/home/inbox/inbox.component.ts
@@ -1,16 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, AfterViewInit, ChangeDetectorRef, Component, DoCheck, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Notification } from '../../shared/models/notification.model';
 import { CommonModule } from '@angular/common';
 import { NotificationComponent } from './notification/notification.component';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-inbox',
   standalone: true,
-  imports: [CommonModule, NotificationComponent],
+  imports: [CommonModule, NotificationComponent, LucideAngularModule],
   templateUrl: './inbox.component.html',
   styleUrl: './inbox.component.scss'
 })
-export class InboxComponent {
+export class InboxComponent implements AfterViewInit {
   @Input() notifications: Notification[] = [];
+  @ViewChild('notificationContainer') notificationContainer: ElementRef | undefined;
+
+  public showScrollArrow: boolean = true;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.onScroll();
+    this.changeDetectorRef.detectChanges();
+  }
+
+  public onScroll(): void {
+    const navContainer = this.notificationContainer?.nativeElement;
+    const maxScroll = navContainer.scrollHeight - navContainer.clientHeight;
+
+    if(navContainer.clientHeight < 500){
+      this.showScrollArrow = false;
+      return;
+    }
+    
+    this.showScrollArrow = navContainer.scrollTop < maxScroll - 10;
+  }
 
 }
