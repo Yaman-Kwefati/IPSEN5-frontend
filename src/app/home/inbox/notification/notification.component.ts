cfecb02317ff95e3855938cb0c7701766bc3c4b5
Amber de Geus <s1143282@student.hsleiden.nl>
2024-05-07 14:08:37 +0200

    Feat: added methods for accepting and refusing request

--- src/app/home/inbox/notification/notification.component.ts
+++ src/app/home/inbox/notification/notification.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Notification } from '../../../shared/models/notification.model';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule } from 'lucide-angular';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-notification',
@@ -12,5 +13,18 @@ import { LucideAngularModule } from 'lucide-angular';
 })
 export class NotificationComponent {
   @Input() notification!: Notification;
+
+  constructor(private toastr: ToastrService){}
+
+  onRefuseRequest(): void {
+    this.toastr.success('Je hebt het verzoek om van ruimte te wisselen afgewezen.', 'Success')
+    // TODO: implementation of refusing request 
+
+  }
+
+  onAcceptRequest(): void {
+    this.toastr.success('Je hebt het verzoek om van ruimte te wisselen geaccepteerd', 'Success')
+    // TODO: implementation of accepting request
+  }
 
 }
