056aaaebb861681b82c4ad32085d55a7a231f390
Eren <e.basaran2004@gmail.com>
2024-06-01 17:05:41 +0200

    Feat: Admin can now filter users based on email, name and phone number

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,30 +2,57 @@ import { Component } from '@angular/core';
 import {UserService} from "../shared/service/requests/user.service";
 import {User} from "../shared/model/user.model";
 import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: '' +
     'app-users',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
   public users: User[] = []
+  public usersToShow: User[] = []
+  public searchInput: String = '';
+
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe(users => {
       this.users = users;
+      this.usersToShow = users;
       console.log(this.users)
     });
   }
 
-  onCreateClick() {
+  public onCreateClick() {
 
   }
+
+  public onInputEdit(event: any) {
+    this.searchInput = event;
+
+    if (this.searchInput === '') {
+      this.usersToShow = this.users;
+      return
+    }
+
+    this.usersToShow = this.users.filter(user => {
+      let fullName = user.firstName.toLowerCase() + " " + user.lastName.toLowerCase();
+      let phone = user.phoneNumber.toLowerCase();
+      let search = this.searchInput.toLowerCase();
+      return user.email.toLowerCase().includes(search) || fullName.includes(search) || phone.includes(search)
+    });
+
+
+  }
+
+
+
 }
