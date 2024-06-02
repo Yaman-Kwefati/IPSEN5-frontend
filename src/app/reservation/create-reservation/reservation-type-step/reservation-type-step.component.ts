dfb77e4d4b86720744e06e41923a716bfa068b89
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2024-06-02 21:45:19 +0200

    Feat: Added formcontrol for type step

--- src/app/reservation/create-reservation/reservation-type-step/reservation-type-step.component.ts
+++ src/app/reservation/create-reservation/reservation-type-step/reservation-type-step.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {ReservationType} from "../../../shared/model/reservering-type.enum";
 import {MatStepperNext, MatStepperPrevious} from "@angular/material/stepper";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reservation-type-step',
@@ -13,12 +14,20 @@ import {MatStepperNext, MatStepperPrevious} from "@angular/material/stepper";
   styleUrl: './reservation-type-step.component.scss'
 })
 export class ReservationTypeStepComponent {
-    @Output() reservationType = new EventEmitter<ReservationType>();
-    protected readonly ReservationType = ReservationType;
-    protected selectedType!: ReservationType;
+  typeFormGroup: FormGroup;
+  @Output() reservationType = new EventEmitter<ReservationType>();
+  protected readonly ReservationType = ReservationType;
+  protected selectedType!: ReservationType;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.typeFormGroup = this._formBuilder.group({
+      reservationType: new FormControl(null, Validators.required)
+    });
+  }
 
   protected setReservationType(type: ReservationType): void {
     this.selectedType = type;
     this.reservationType.emit(type);
+    this.typeFormGroup.get('reservationType')!.setValue(type);
     }
 }
