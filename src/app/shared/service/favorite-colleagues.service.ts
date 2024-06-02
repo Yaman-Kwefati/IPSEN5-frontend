c3f877d8ecc28c3ea6083a4cbb891a84f7faa96e
Stan <s1136644@student.hsleiden.nl>
2024-06-02 23:00:32 +0200

    unfinished feat: add favorite location

--- src/app/shared/service/favorite-colleagues.service.ts
+++ src/app/shared/service/favorite-colleagues.service.ts
@@ -1,7 +1,45 @@
-import {Injectable} from "@angular/core";
+import { Injectable } from '@angular/core';
+import { ApiResponse, ApiService } from './api.service';
+import { Building } from '../model/building.model';
+import { Observable, catchError } from 'rxjs';
+import { error } from 'cypress/types/jquery';
+import { Wing } from '../model/wing.model';
+import { Floor } from '../model/floor.model';
 
-@Injectable()
-export class FavoriteColleaguesService{
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoriteColleaguesService {
   //TODO
 
+  constructor(private apiService: ApiService) {}
+
+  public getBuildingList(): Observable<ApiResponse<Building[]>> {
+    return this.apiService.get<ApiResponse<Building[]>>('/building').pipe(
+      catchError((error) => {
+        console.error('Error fetching building list: ', error);
+        throw error;
+      })
+    );
+  }
+
+  public getWingList(buildingId: string): Observable<ApiResponse<Wing[]>> {
+    return this.apiService
+      .get<ApiResponse<Wing[]>>(`/building/${buildingId}/wing`)
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching wing list: ', error);
+          throw error;
+        })
+      );
+  }
+
+  public getAllFloors(): Observable<ApiResponse<Floor[]>> {
+    return this.apiService.get<ApiResponse<Floor[]>>('/floor').pipe(
+      catchError((error) => {
+        console.error('Error fetching floor list: ', error);
+        throw error;
+      })
+    );
+  }
 }
