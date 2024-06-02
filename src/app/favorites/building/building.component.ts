4279cc6854b2202b2a450596af437dc29009747c
Stan <s1136644@student.hsleiden.nl>
2024-06-02 20:34:18 +0200

    fix: merge old branch into new branch

---
+++ src/app/favorites/building/building.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UserService } from '../../shared/service/requests/user.service';
+import { User } from '../../shared/model/user.model';
+import { CommonModule, NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SearchPipe } from '../../shared/pipe/searchItem.pipe';
+
+@Component({
+  selector: 'app-building',
+  standalone: true,
+  imports: [FormsModule, RouterLink, NgForOf, FontAwesomeModule, CommonModule],
+  templateUrl: './building.component.html',
+})
+export class BuildingComponent implements OnInit {
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    console.log('BuildingComponent initiated');
+  }
+}
