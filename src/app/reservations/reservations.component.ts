60f790935ff64137e2c6aa900ed3f07cc2a139d8
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2024-06-03 15:40:25 +0200

    Feature: added reservations list

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,17 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DeleteModalComponent} from "./delete-modal/delete-modal.component";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {Reservation} from "../shared/model/reservation.model";
+import {ReservationService} from "../shared/service/reservation.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
   imports: [
-    DeleteModalComponent
+    DeleteModalComponent,
+    MatExpansionModule
   ],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.scss'
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit{
   isDeleteModalVisible: boolean = false;
+  protected reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService,
+              private toastr: ToastrService){
+  }
+
+  ngOnInit(): void {
+    this.reservationService.getAllReservations2().subscribe(
+      data => {
+        this.reservations = data.payload;
+        console.log(this.reservations);
+      }, error => {
+        if (error && (error as any).error) {
+          this.toastr.error((error as any).error.message);
+        } else {
+          this.toastr.error('Fout bij het ophalen van reserveringen');
+        }
+      }
+    );
+  }
+
+  getDate(dateToFormat: Date) {
+    const date = new Date(dateToFormat);
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  }
+
+  getTime(dateToFormat: Date) {
+    const date = new Date(dateToFormat);
+    return `${date.getHours()}:${date.getMinutes()}`;
+  }
 
   openDeleteModal() {
     this.isDeleteModalVisible = !this.isDeleteModalVisible;
