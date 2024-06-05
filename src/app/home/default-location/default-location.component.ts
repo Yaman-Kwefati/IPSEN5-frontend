404f124306a88968c3259925306e6d8683897122
Stan <s1136644@student.hsleiden.nl>
2024-06-05 19:47:06 +0200

    fix: add homepage standard location

--- src/app/home/default-location/default-location.component.ts
+++ src/app/home/default-location/default-location.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { StandardLocation } from '../../shared/model/standard-location.model';
+import { Wing } from '../../shared/model/wing.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-default-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './default-location.component.html',
   styleUrl: './default-location.component.scss'
 })
 export class DefaultLocationComponent {
-  @Input() defaultLocation!: StandardLocation;
+  @Input() defaultLocation!: Wing;
 
 }
